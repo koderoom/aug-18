1d2466d13164c4ec3a9e68e9ab79ead80c97f9db
santosh <santosh.ece06@gmail.com>
2018-10-19 21:56:43 +0530

    day 5

--- src/app-service.js
+++ src/app-service.js
@@ -59,6 +59,65 @@ appModule.service("DATA_SERVICE", [function(){
 
     
 
+    // DAY - 5
+    const cardDay5 = {
+        "ID" : 5,
+        "TITLE" : "DAY 5",
+        "HEADING" : "Dynamic blocks",
+        "DESC" : `CSS Combinator rule and fundamentals of javascript.`,
+
+        "DEMO_LIST" : [
+            {  
+                "ID":1, "TITLE": "CSS Combinator Rule example.", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/1.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/1.html`,
+            },
+
+            {  
+                "ID":2, "TITLE": "setInterval() basic example.", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/2.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/2.html`,
+            },
+
+            {  
+                "ID":3, "TITLE": "setInterval() basic, change background.", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/3.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/3.html`,
+            },
+
+            {  
+                "ID":4, "TITLE": "setInterval() basic, change background every second", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/4.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/4.html`,
+            },
+
+            {  
+                "ID":5, "TITLE": "Counter Example", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/6.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/6.html`,
+            },
+
+            {  
+                "ID":6, "TITLE": "Timer Example", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/7.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/7.html`,
+            },
+
+            {  
+                "ID":7, "TITLE": "Bootstrap Layout Example", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/9.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/9.html`,
+            },
+
+            {  
+                "ID":7, "TITLE": "Dynamic blocks with setInterval()", 
+                "DEMO_LINK" : `https://aug-18.github.io/dacdemo/day5/10.html`,
+                "SOURCE_LINK" : `https://github.com/aug-18/dacdemo/blob/master/day5/10.html`,
+            },
+        ]
+    };
+    CARD_LIST.push(cardDay5);
+
 
     this.CARD_LIST = CARD_LIST;
 
