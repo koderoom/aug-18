91d6224c4d99eb08b52df3a5be045729dd281f32
santosh <santosh.ece06@gmail.com>
2018-10-20 16:15:59 +0530

    minification updat.e

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const app=angular.module("app", []);
 
-app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
+app.filter('unsafe', ["$sce", function($sce) { 
+    return $sce.trustAsHtml; 
+}]);
 
 app.controller("spa", ["$scope", "$sce", "DATA_SERVICE" , function($scope, $sce, DATA_SERVICE){
     $scope.title = "CDACIAN AUGUST";
 
     $scope.CARD_LIST = DATA_SERVICE.CARD_LIST;
-
     
 }]);
 
