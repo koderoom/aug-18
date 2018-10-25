5f961d807f36dbacaf3f50f91b8815f7ad9cf16d
santosh <santosh.ece06@gmail.com>
2018-10-25 17:23:18 +0530

    dynamic clor.

--- src/app.js
+++ src/app.js
@@ -7,8 +7,17 @@ app.filter('unsafe', ["$sce", function($sce) {
 app.controller("spa", ["$scope", "$sce", "DATA_SERVICE" , function($scope, $sce, DATA_SERVICE){
     $scope.title = "CDACIAN AUGUST";
 
+    $scope.bgList= [{"ID":1, "BG": "bg-primary"}, {"ID":2, "BG": "bg-danger"}, {"ID":3, "BG": "bg-dark"}];
+    $scope.bgRef = $scope.bgList[2];
+
     $scope.CARD_LIST = DATA_SERVICE.CARD_LIST;
-    
+
+
+    $scope.uilayout = function(bgRef){
+        bgRef = bgRef || $scope.bgList[2];
+
+        $scope.bgRef = bgRef;
+    }
 }]);
 
 
