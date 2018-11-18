33a3bbcbdbb6e850b508bc1b3e84afea1a30c2e1
santosh <santosh.ece06@gmail.com>
2018-11-18 17:19:58 +0530

    update

--- src/app.js
+++ src/app.js
@@ -48,3 +48,27 @@ app.controller("home", ["$scope", "$sce", "DATA_SERVICE" , function($scope, $sce
 
 
 
+app.controller("dacexam", ["$scope", "$sce", "DATA_SERVICE", "DATA_SERVICE_EXAM" , function($scope, $sce, DATA_SERVICE, DATA_SERVICE_EXAM){
+    let title = "AUGUST-18";
+    let bgList= [{"ID":1, "BG": "bg-primary", "BTN":"btn-primary"}, {"ID":2, "BG": "bg-danger", "BTN":"btn-danger"}, {"ID":3, "BG": "bg-dark", "BTN":"btn-dark"}, {"ID":3, "BG": "bg-info", "BTN":"btn-info"}];
+    let randomIndex = Math.floor((Math.random() * 4));
+
+    $scope.init = (function(){
+        $scope.title = title;
+        $scope.bgList = bgList;    
+
+        $scope.bgRef = bgList[randomIndex];
+
+        // CARD DATA
+        $scope.EXAM_LIST = DATA_SERVICE_EXAM.EXAM_LIST;
+    })();
+
+    
+    $scope.uilayout = function(bgRef){
+        bgRef = bgRef || bgList[randomIndex];
+
+        $scope.bgRef = bgRef;
+    }
+}]);
+
+
