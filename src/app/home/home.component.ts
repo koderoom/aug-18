b1bf9c35ad9de2e5639f07a2ecd7a39f97f11a12
santosh <santosh.ece06@gmail.com>
2018-11-19 18:09:35 +0530

    home compoenent data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,42 @@ import { DataexamService } from 'src/app/common/dataexam.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
+  title = "AUG-18";
+  bgRef:any;
+  bgList = [{"ID":1, "BG": "bg-primary", "BTN":"btn-primary"}, {"ID":2, "BG": "bg-danger", "BTN":"btn-danger"}, {"ID":3, "BG": "bg-dark", "BTN":"btn-dark"}, {"ID":3, "BG": "bg-info", "BTN":"btn-info"}];
+  randomIndex = Math.floor((Math.random() * 4));
+
+  CARD_LIST:any[];
+  FEATURED_LIST:any[];
+  FEATURED_ANGULAR_LIST:any[];
+
+  BASE_URL_DEMO:string;
+  EXAM_LIST:any[];
+  TOPLIST:any[];
 
   constructor(
-    data: DataService,
-    dataexam: DataexamService
-  ) { 
-    console.log(dataexam.EXAM_LIST);
+    public data: DataService,
+    public dataexam: DataexamService
+  ) { }
+
+  ngOnInit(): void {
+    this.bgRef = this.bgList[this.randomIndex];
+
+    this.CARD_LIST = this.data.CARD_LIST;
+    this.FEATURED_LIST = this.data.FEATURED_LIST;
+    this.FEATURED_ANGULAR_LIST = this.data.FEATURED_ANGULAR_LIST;
+
+    this.BASE_URL_DEMO = this.dataexam.BASE_URL_DEMO;
+    this.EXAM_LIST = this.dataexam.EXAM_LIST;
+    this.TOPLIST = this.dataexam.B2LIST.concat(this.dataexam.B1LIST);
   }
 
   
+  uilayout(bgRef) {
+    bgRef = bgRef || this.bgList[this.randomIndex];
+    this.bgRef = bgRef;
+  }
 
 }
