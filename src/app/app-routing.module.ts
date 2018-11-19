fa13b8051d83c4254e4cdaebd479e043a985c64e
santosh <santosh.ece06@gmail.com>
2018-11-19 17:35:08 +0530

    component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DacexamComponent } from './dacexam/dacexam.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [HomeComponent, DacexamComponent]
 })
 export class AppRoutingModule { }
---
+++ src/app/dacexam/dacexam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dacexam',
+  templateUrl: './dacexam.component.html',
+  styleUrls: ['./dacexam.component.css']
+})
+export class DacexamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
